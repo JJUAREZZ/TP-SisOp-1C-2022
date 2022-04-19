762389608f8c8dd34ae1dbd751669aaacc599bc7
Kenti <mopup2017@gmail.com>
2022-04-18 22:43:45 -0300

    termino parser

--- consola/consola.c
+++ consola/consola.c
@@ -1,23 +1,23 @@
 #include "utils.h"
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
 
-	int tamanio_proceso= atoi(argv[1]);
-	char* path_instrucciones= (char*) argv[2];
-	char ** lista;
+	int tamanio_proceso = atoi(argv[1]);
+	char* path_instrucciones = (char*) argv[2];
 
-	printf("tamanio en bytes recibido: %d \n", tamanio_proceso);
-	printf("path recibido: %s", path_instrucciones);
+	printf("\nTamanio en bytes recibido: %d \n", tamanio_proceso);
+	printf("Path recibido: %s", path_instrucciones);
 	printf("\n");
 
-	crear_lista_de_instrucciones(path_instrucciones,&lista);
+	t_list *inst_list = crear_lista_de_instrucciones(path_instrucciones);
 
+	//Muestro los Id de las instrucciones
+	for (int i = 0; i < (inst_list->elements_count); i++) {
+		instr_t *instruction = list_get(inst_list, i);
+		printf("\n%s", instruction->id);
 
-	for(int i=0 ; i<5; i++)
-		printf("\n%s", lista[i]);
+	}
 
-	printf("\n");
-
-	 return 0;
+	return 0;
 
 }
--- consola/utils.h
+++ consola/utils.h
@@ -1,40 +1,60 @@
+#ifndef UTIL_H_
+#define UTIL_H_
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <commons/log.h>
-#include<commons/string.h>
+#include <commons/string.h>
+#include <commons/collections/list.h>
 
-
-
-typedef struct{
+typedef struct {
 	char* id;
-	unsigned int numero_de_parametros;
-	unsigned int *parametros;
-}instr_t;
+	t_list *param;
+} instr_t;
 
-void crear_lista_de_instrucciones(char *path, char ***list) {
+t_list * crear_lista_de_instrucciones(char *path) {
 	FILE* file = fopen(path, "r");
 	struct stat stat_file;
-		stat(path, &stat_file);
+	stat(path, &stat_file);
 
 	if (file == NULL) {
 		return NULL;
 	}
 
-	char* buffer= calloc(1, stat_file.st_size + 1);
+	char* buffer = calloc(1, stat_file.st_size + 1);
 	fread(buffer, stat_file.st_size, 1, file);
 
-	for(int i=1;!feof(file);i++){
+	for (int i = 1; !feof(file); i++) {
 		fread(buffer, stat_file.st_size, 1, file);
 	}
 
-	*list= string_split(buffer,"\n");
+	char **inst_per_line = string_split(buffer, "\n");
+
+	t_list *inst_list = list_create();
+
+	void closure(char *line) {
+		char** inst_and_param = string_split(line, " ");
+		instr_t *single_inst = malloc(sizeof(instr_t));
+		single_inst->param = list_create();
+		single_inst->id = inst_and_param[0];
+		for (int i = 1; inst_and_param[i] != NULL; i++) {
+			list_add(single_inst->param, inst_and_param[i]);
+		}
+
+		list_add(inst_list, single_inst);
+	}
+
+	string_iterate_lines(inst_per_line, closure);
 
 	free(buffer);
+	free(inst_per_line);
 	fclose(file);
 
-	return NULL;
+	return inst_list;
 
 }
+
+#endif /* UTIL_H_ */
 
