d070b9591e24e8ae361f68e503c0f67b0d246097
MartinY <mrtnyoo@gmail.com>
2022-04-24 13:01:03 -0300

    Subo Shared Library

---
+++ shared-messirve/src/connect.h
@@ -0,0 +1,33 @@
+/*
+ * connect.h
+ *
+ *  Created on: 23 abr. 2022
+ *      Author: utnso
+ */
+
+#ifndef SRC_CONNECT_H_
+#define SRC_CONNECT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <fcntl.h>
+
+
+#define BACKLOG 10
+
+int socket_create_listener(char* ip, int port);
+int socket_connect_to_server(char* ip, int port);
+
+
+#endif /* SRC_CONNECT_H_ */
