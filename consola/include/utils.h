0d5aafbcd1a0ea37643a933aa7325c085c9d7d93
Kenti <mopup2017@gmail.com>
2022-05-11 16:55:52 -0300

    refactor parser: almacena lo leido en una lista de instr_t y los parametros se almacenan en un array

--- consola/include/utils.h
+++ consola/include/utils.h
@@ -9,44 +9,50 @@
 #include <commons/string.h>
 #include <commons/config.h>
 #include <commons/collections/list.h>
+#include "../../shared/include/serializacion.h"
 
 typedef struct {
 	char* id;
-	t_list *param;
+	uint32_t nroDeParam;
+	uint32_t *param;
 } instr_t;
+
 
 t_list * crear_lista_de_instrucciones(char *path) {
 	FILE* file = fopen(path, "r");
 	struct stat stat_file;
 	stat(path, &stat_file);
-
 	if (file == NULL) {
 		return NULL;
 	}
-
 	char* buffer = calloc(1, stat_file.st_size + 1);
 	fread(buffer, stat_file.st_size, 1, file);
-
 	for (int i = 1; !feof(file); i++) {
 		fread(buffer, stat_file.st_size, 1, file);
 	}
 
-	char **inst_per_line = string_split(buffer, "\n");
 
-	t_list *inst_list = list_create();
-
-	void closure(char *line) {
-		char** inst_and_param = string_split(line, " ");
-		instr_t *single_inst = malloc(sizeof(instr_t));
-		single_inst->param = list_create();
-		single_inst->id = inst_and_param[0];
-		for (int i = 1; inst_and_param[i] != NULL; i++) {
-			list_add(single_inst->param, inst_and_param[i]);
-		}
-
-		list_add(inst_list, single_inst);
+	char **inst_per_line = string_split(buffer, "\n"); //divido el archivo leido por lineas
+	int tamanioArray(char ** array){
+		int n=0;
+		for(int i=0 ;*(array+i)!= NULL; i++)
+			n++;
+		return n;
 	}
 
+	t_list *inst_list = list_create();                 //creo la lista de instrucciones
+	
+	void closure(char *line) {
+	    char** inst_and_param = string_split(line, " ");    //divido la linea por " "
+		instr_t *single_inst = malloc(sizeof(instr_t));     //reservo memoria para estructura instr_t
+	    single_inst->id= strdup(inst_and_param[0]);        //copio el id de la instruccion
+	    single_inst->nroDeParam= tamanioArray(inst_and_param)-1;     //guardo la cantidad de parametros 
+	    single_inst->param= malloc(sizeof(uint32_t)*single_inst->nroDeParam);  //reservo mem para los parametros
+		for (int i = 1; i<=single_inst->nroDeParam; i++) {   //recorro el array a partir del id de la instruccion
+			single_inst->param[i-1]= (uint32_t) atoi(inst_and_param[i]);	//copio los parametros
+		}
+		list_add(inst_list, single_inst);                   //agrego la instruccion a la lista
+	}
 	string_iterate_lines(inst_per_line, closure);
 
 	free(buffer);
@@ -55,6 +61,33 @@ t_list * crear_lista_de_instrucciones(char *path) {
 
 	return inst_list;
 
+/*
+	void closure(char *line) {
+		char** inst_and_param = string_split(line," ");
+		instr_t *single_inst = malloc(sizeof(instr_t));
+		single_inst.id= malloc(sizeof(char)*(strlen(*single_inst)+1));
+		memcpy(single_inst.id,*single_inst, strlen(*single_inst)+1);
+
+		int i;
+		for(i =1; inst_and_param[i] != NULL; i++){
+			single_inst.param= realloc(single_inst.param,(sizeof(uint32_t)*i)+1);
+			single_inst.param[i-1]= (uint32_t) atoi(inst_and_param[i]);
+		}
+		single_inst.param[i]= '\0';	
+	}
+*/
+/*
+void closure(char *line) {
+		char** inst_and_param = string_split(line, " ");
+		instr_t *single_inst = malloc(sizeof(instr_t));
+		single_inst->param = list_create();
+		single_inst->id = inst_and_param[0];
+		for (int i = 1; inst_and_param[i] != NULL; i++) {
+			list_add(single_inst->param, (uint32_t) atoi(inst_and_param[i]));
+		}
+		list_add(inst_list, single_inst);
+	}
+*/
 }
 
 typedef struct {
--- consola/src/consola.c
+++ consola/src/consola.c
@@ -5,6 +5,7 @@ int main(int argc, char** argv) {
 
 	int tamanio_proceso = atoi(argv[1]);
 	char* path_instrucciones = (char*) argv[2];
+	int conexion;
 
 	printf("\nTamanio en bytes recibido: %d \n", tamanio_proceso);
 	printf("Path recibido: %s", path_instrucciones);
@@ -13,16 +14,18 @@ int main(int argc, char** argv) {
 	t_list *inst_list = crear_lista_de_instrucciones(path_instrucciones);
 
 	//Muestro los Id de las instrucciones
-	for (int i = 0; i < (inst_list->elements_count); i++) {
-		instr_t *instruction = list_get(inst_list, i);
-		printf("\n%s", instruction->id);
-
+	void closure(instr_t* element){
+		printf("\n%s",element->id);
+		for(int i=0; i<element->nroDeParam;i++){
+			printf(" %d",(int) element->param[i]);
+		}
 	}
-
+	list_iterate(inst_list,closure);
+	
 	cargar_configuracion();
-	socket_connect_to_server(config_valores.ip_kernel, config_valores.puerto_kernel);
+	conexion= socket_connect_to_server(config_valores.ip_kernel, config_valores.puerto_kernel);
+	//paquete(conexion);
 
-	return 0;
 
 
 }
