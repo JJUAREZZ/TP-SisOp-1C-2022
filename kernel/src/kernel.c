4177648ae70e93c9d16bf06d62617e786617b603
mspagnol@frba.utn.edu.ar <mspagnol@frba.utn.edu.ar>
2022-05-27 17:33:21 -0300

    Comenzando planificador a largo plazo

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -5,23 +5,6 @@
 
 int main(){
 
-	//Estado de planificador;
-	t_list* estadoNew;
-	t_list* estadoBlock;
-	t_list* estadoBlockSusp;
-	t_list* estadoReadySusp;
-	t_list* estadoExec;
-	t_list* estadoExit;
-	t_list* estadoReady;
-	
-	estadoNew 	= list_create();
-	estadoReady = list_create();
-	estadoBlock = list_create();
-	estadoBlockSusp = list_create();
-	estadoReadySusp = list_create();
-	estadoExec = list_create();
-	estadoExit = list_create();
-
 	//create_kernel_logger();
 	load_configuration();
 	kernel_server_init();
