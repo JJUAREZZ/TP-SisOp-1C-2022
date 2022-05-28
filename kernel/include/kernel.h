0934a5246d658a3afce6094bd7e3b6ac636bec03
Kenti <mopup2017@gmail.com>
2022-05-28 13:57:10 -0300

    borro comments

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -9,113 +9,17 @@
 #include <commons/config.h>
 #include <commons/collections/list.h>
 
-/*
-int create_kernel_logger(){
-	int response = kernel_logger_create("kernel_logger.log");
-	if(response<0){
-		free(config_valores->ip_kernel);
-		free(config_valores->puerto_kernel);
-		free(config_valores);
-	
-	}
-	else{
-		kernel_logger_info("Kernel Logger creado");
-	}
-}
-*/
 void *conectarse_con_consola();
 void *recibir_proceso(int);
 
 t_log *logger;
 
 void kernel_server_init(){
-/*
-	pthread_attr_t attrs;
-	pthread_attr_init(&attrs);
-	pthread_attr_setdetachstate(&attrs, PTHREAD_CREATE_JOINABLE);
-*/
+
 	logger = log_create("log.log", "Servidor", 1, LOG_LEVEL_DEBUG);
 	pthread_t conexion_con_consola;
 	pthread_create(&conexion_con_consola, NULL, conectarse_con_consola, NULL); //HILO PRINCIPAL 
 	pthread_join(conexion_con_consola, NULL);
-/*
-	int accepted_fd;
-	for (;;) {
-		pthread_t tid;
-		if ((accepted_fd = accept(kernel_socket,(struct sockaddr *) &client_info, &addrlen)) != -1){
-			//kernel_logger_info("Creando un hilo para atender una conexión en el socket %d", accepted_fd);
-			log_info(logger,"Creando un hilo para atender una conexión en el socket %d", accepted_fd);
-		}
-		else{
-			log_info(logger,"Error al conectar con un cliente");
-		}
-		t_proceso* proceso= malloc(sizeof(t_proceso));
-		pcb* pcbNuevo= malloc(sizeof(pcb)); 
-		estadoNew 	= list_create();
-		estadoReady = list_create();
-		estadoBlock = list_create();
-		estadoBlockSusp = list_create();
-		estadoReadySusp = list_create();
-		estadoExec = list_create();
-		estadoExit = list_create();	
-		int cod_op;
-		while (cod_op= recibir_operacion(accepted_fd)>0) {
-			switch (cod_op) {
-			case PAQUETE:
-				proceso = recibir_paquete(accepted_fd);
-				log_info(logger,"Me llego el siguiente proceso:\n");
-				printf("Tamanio del Proceso en bytes: %d", proceso->tamanio);
-				printf("\nInstrucciones : \n");
-				void mostrarInstrucciones(instr_t* element)
-				{
-					printf("%s ",element->id);
-					for(int i=0; i<element->nroDeParam;i++)
-						printf(" %d",(int) element->param[i]);
-					printf("\n");
-				}
-				list_iterate(proceso->instrucciones, mostrarInstrucciones);
-
-				//Crear Hilo para crear PCB
-				pthread_t hiloCreaPcb;
-				pthread_t hiloPcbANew;
-				pthread_t hiloEnviarAReady;
-
-				t_list *arg= list_create();
-				list_add(arg,proceso);
-				list_add(arg,pcbNuevo);
-				list_add(arg,logger);
-
-				t_list *arg1= list_create();
-				list_add(arg1,estadoNew);
-				list_add(arg1,logger);
-
-				crearPcb(arg);
-				enviarAReady(arg1);
-				//pthread_create(&hiloCreaPcb, NULL, crearPcb, arg);
-				//pthread_create(&hiloPcbANew, NULL, (void*) list_add(estadoNew, pcbNuevo), NULL);
-				//pthread_create(&hiloEnviarAReady, NULL, enviarAReady, (t_list*) arg1);
-
-				//Extrae elemento de New y envia a READY
-				//****FALTA MODIFICAR EL CAMPO TABLA DE PAGINAS****
-				//thread_t hiloEnviarAReady;
-				//pthread_create(hiloEnviarAReady, NULL, enviarAReady(estadoNew, logger), NULL);
-
-				//QUE SE EJECUTE UN HILO A LA VEZ
-				//pthread_join(hiloCreaPcb, NULL);
-				//pthread_join(hiloPcbANew, NULL);
-				//pthread_join(hiloEnviarAReady, NULL);
-
-				break;
-			case -1:
-				log_info(logger,"el cliente se desconecto. Terminando servidor");
-				return;
-			default:
-				log_info(logger,"Operacion desconocida. No quieras meter la pata");
-				break;
-			}
-		}
-	}
-	*/
 	
 }
 
