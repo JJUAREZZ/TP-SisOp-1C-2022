1253426cc6e96a242ec6e46c62640b1f27fe41d3
Kenti <mopup2017@gmail.com>
2022-07-13 22:57:10 -0300

    Arreglo liberarPcb(), disminuyo memory leaks de kernel

--- shared/src/estructuras.c
+++ shared/src/estructuras.c
@@ -3,13 +3,13 @@
 
 void liberarPcb(pcb* pcbALiberar)
 {
+    
     void liberarListaDeInstrucciones(instr_t* instruccion)
     {
         free(instruccion->id);
         free(instruccion->param);
         free(instruccion);
     }
-    list_iterate(pcbALiberar->instr,liberarListaDeInstrucciones);
-    list_destroy(pcbALiberar->instr);
+    list_destroy_and_destroy_elements(pcbALiberar->instr, liberarListaDeInstrucciones);
     free(pcbALiberar);
 }
