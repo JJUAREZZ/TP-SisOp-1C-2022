16e3bb71addb14a20fefbaee7c6014971cc4852a
tinSpagnol <102607747+mSpagnol@users.noreply.github.com>
2022-07-15 16:24:51 -0300

    Update kernel.c

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,9 +1,11 @@
 #include "../include/kernel.h"
+#include "../include/utils.h"
 #include <commons/config.h>
 #include <commons/log.h>
 #include <commons/collections/list.h>
 
 int main(int argc, char** argv) {
+	logger = log_create("log.log", "Servidor", 1, LOG_LEVEL_DEBUG);
 	path_kernel_config = (char*) argv[1];
 	load_configuration();
 	kernel_server_init(); 
